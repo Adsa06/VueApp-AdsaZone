c54b36026c9b453034819c001e6e03f0d533863f
Aitor de Santos Amoros <soyadsa06@gmail.com>
2025-01-13 23:22:10 +0100

    Primeras conexiones a firebase

--- VueApp-AdsaZone/src/main.js
+++ VueApp-AdsaZone/src/main.js
@@ -2,5 +2,17 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import { VueFire, VueFireAuth } from 'vuefire'
+import { firebaseApp } from './firebase'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(VueFire, {
+    // imported above but could also just be created here
+    firebaseApp,
+    modules: [
+        // we will see other modules later on
+        VueFireAuth(),
+    ],
+})
+
+app.mount('#app')
