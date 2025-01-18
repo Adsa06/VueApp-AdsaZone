7257653ecf50aa7748113c7c69a62b84804e1072
Aitor de Santos Amoros <soyadsa06@gmail.com>
2025-01-18 18:52:02 +0100

    Perfil del usuario terminado

--- VueApp-AdsaZone/src/components/PerfilConfig/Perfil.vue
+++ VueApp-AdsaZone/src/components/PerfilConfig/Perfil.vue
@@ -18,9 +18,9 @@ import Card from 'primevue/card';
 const auth = getAuth();
 const bbdd = useFirestore();
 
-const name = ref('');
-const photoPerfil = ref('');
-const fechaCreacionCuenta = ref('');
+const name = ref(auth.currentUser.displayName);;
+const photoPerfil = ref(auth.currentUser.photoURL);
+const fechaCreacionCuenta = ref(auth.currentUser.metadata.creationTime);
 const edad = ref('');
 const sobreMi = ref('');
 
@@ -57,11 +57,11 @@ onMounted(() => {
             <Card>
                 <template #content>
                     <div class="ParteSuperior">
-                        <Avatar image="/images/avatar/amyelsner.png" class="mr-2" size="xlarge" shape="circle" />
+                        <Avatar :image="photoPerfil" class="mr-2" size="xlarge" shape="circle" />
                         <div class="NameAgeDate">
-                            <h1>Adsa</h1>
-                            <h2>Edad del usuario: 17 años</h2>
-                            <h3>Fecha de creacion de la cuenta: 18/05/2005</h3>
+                            <h1>{{ name }}</h1>
+                            <h2>Edad del usuario: {{ edad }}</h2>
+                            <h3>Fecha de creacion de la cuenta: {{ fechaCreacionCuenta }}</h3>
                         </div>
                     </div>
                 </template>
@@ -70,38 +70,9 @@ onMounted(() => {
         <div >
             <Fieldset legend="Sobre Mi" class="SobreMi">
                 <div >
-                    <ScrollPanel style="width: 100%; max-height: 25rem">
+                    <ScrollPanel style="width: 100%; min-height: 3.5rem; max-height: 25rem">
                         <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                        </p>
-                        <p>
-                            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam
-                            voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Consectetur, adipisci velit, sed quia non numquam eius modi.
-                        </p>
-                        <p>
-                            At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui
-                            officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus.
-                        </p>
-                        <p class="m-0">
-                            Quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non
-                            recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat
-                        </p>                    
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                        </p>
-                        <p>
-                            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam
-                            voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Consectetur, adipisci velit, sed quia non numquam eius modi.
-                        </p>
-                        <p>
-                            At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui
-                            officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus.
-                        </p>
-                        <p class="m-0">
-                            Quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non
-                            recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat
+                            {{ sobreMi }}
                         </p>
                     </ScrollPanel>
                 </div>
